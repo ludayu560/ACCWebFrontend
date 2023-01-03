cb0d7ec111b4bb5ecff4ce12bce50d15e008c2af
Shonn Li <77355994+Shonn-Li@users.noreply.github.com>
2023-01-03 09:45:00 -0800

    backend setup

--- Backend/BackendAPI/RestAPI/view.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-@api_view(['GET'])
-def getDate(request):
-    person = {'name': 'Dennis', 'age':28}
-    return Response(person)
