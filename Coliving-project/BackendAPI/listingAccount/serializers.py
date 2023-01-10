caaf6116b90a9395b68ef492ec3da3eb302f5bde
Wesley Kim <71526260+wesleykim2002@users.noreply.github.com>
2023-01-10 15:34:45 -0500

    Revert "implemented listingAccount"
    This reverts commit e4ba1241252c44b5e8ab1ee006be23b13c8f7202.

--- Coliving-project/BackendAPI/listingAccount/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from listingAccount.models import ListingAccount
-
-class ListingAccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ListingAccount
-        fields = '__all__'
-        
-        
--- Coliving-project/BackendAPI/listingAccount/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from ..listingAccount import views
-
-urlpatterns = [
-    path('', views.getData),
-    path('add/', views.addListingAccount)
-]
