4c2590f509db2677d16d869d72ff3b39a538264c
George Li <georgeli293@gmail.com>
2023-01-11 11:32:59 -0500

    created listingAccount and removed tenants and homeowners

--- BackendAPI/Coliving/urls.py
+++ BackendAPI/Coliving/urls.py
@@ -19,8 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tenant/', include('tenant.urls')),
-    # path('homeowner/', include('homeowner.urls')),
+    path('listing_account/', include('listingAccount.urls')),
     
     # path('', TemplateView.as_view(template_name='index.html'))
 ]
--- BackendAPI/listingAccount/views.py
+++ BackendAPI/listingAccount/views.py
@@ -3,18 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from tenant.models import Tenant
-from .serializers import TenantSerializer
+from listingAccount.models import ListingAccount
+from .serializers import ListingAccountSerializer
 
 @api_view(['GET'])
 def getData(request):
-    tenants = Tenant.objects.all()
-    serializer = TenantSerializer(tenants, many=True)
+    listingAccount = ListingAccount.objects.all()
+    serializer = ListingAccountSerializer(listingAccount, many=True)
     return Response(serializer.data)
 
 @api_view(['Post'])
-def addTenant(request):
-    serializer = TenantSerializer(data=request.data)
+def addListingAccount(request):
+    serializer = ListingAccountSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer)
