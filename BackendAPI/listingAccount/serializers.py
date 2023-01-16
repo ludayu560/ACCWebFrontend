f781fc4729ce635b53bfb9580b9e4fc048c95412
George Li <georgeli293@gmail.com>
2023-01-16 14:18:53 -0500

    Think i fixed it

--- BackendAPI/listingAccount/serializers.py
+++ BackendAPI/listingAccount/serializers.py
@@ -5,25 +5,23 @@ from listingAccount.models import ListingAccount, PersonalTraits, Interests
 class PersonalTraitsSerializer(serializers.ModelSerializer):
     class Meta:
         model = PersonalTraits    
-        fields = ['traits']
+        fields = "__all__"
 
 class InterestsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Interests
-        fields = ['interests']       
+        fields = "__all__"
 
 class ListingAccountSerializer(serializers.ModelSerializer):
-    personalTraits = PersonalTraitsSerializer(many=True)
-    interests = InterestsSerializer(many=True)
+    personalTraits = PersonalTraitsSerializer(read_only=True,many=True)
+    interests = InterestsSerializer(read_only=True,many=True)
 
     # def create(self, validated_data):
     #     personalTraits_data = validated_data.pop("personalTraits")
     #     listingAccount = ListingAccount.objects.create(**validated_data)
     #     for personalTrait_data in personalTraits_data:
-    #         PersonalTraits.objects.create()
+    #         PersonalTraits.objects.create(listingAccount=listingAccount, personalTrait = **personalTrait_data)
     #     return listingAccount    
     class Meta:
         model = ListingAccount
-        fields = ['first_name', 'last_name', 'email', 'date_of_birth', 
-        'occupation', 'age_range', 'tell_us_about_yourself', 'created',
-        'personalTraits', 'interests']
+        fields = "__all__"
