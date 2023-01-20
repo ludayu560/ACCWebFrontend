1d58d4f8ce94bc4ed9297701684321e0d809b20b
Wesley Kim <wesleykim2002@gmail.com>
2023-01-20 16:46:43 -0500

    populated models and began updating serializers. not complete yet

--- BackendAPI/events/models.py
+++ BackendAPI/events/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Events(models.Model):
+    #image input required
+    event_name = models.CharField(max_length=50, null=True)
+    event_date_time = models.DateTimeField(max_length=50, null=True)
+    event_location = models.CharField(max_length=50, null=True)
+    event_description = models.TextField(null=True)
