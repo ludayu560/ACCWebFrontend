28203da05eb6810259cbabe2d210cbdd44f95010
George Li <georgeli293@gmail.com>
2023-01-20 13:20:45 -0500

    added username to models

--- BackendAPI/listingAccount/models.py
+++ BackendAPI/listingAccount/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class ListingAccount(models.Model):
+    username = models.CharField(max_length=20, null=True)
     first_name = models.CharField(max_length=30, null=True)
     last_name = models.CharField(max_length=30, null=True)
     email = models.EmailField(max_length=254, null=True)
@@ -23,5 +24,5 @@ class Interest(models.Model):
     interest = models.CharField(max_length=200, null=True)
     listing_account = models.ForeignKey(ListingAccount, related_name='interests', on_delete=models.CASCADE, null=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f"{self.interest}"
--- BackendAPI/listingAccount/serializers.py
+++ BackendAPI/listingAccount/serializers.py
@@ -21,11 +21,12 @@ class ListingAccountSerializer(serializers.ModelSerializer):
     interests = serializers.StringRelatedField(many=True)
     class Meta:
         model = ListingAccount
-        fields = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'date_of_birth',
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'phone_number', 'date_of_birth',
                   'location', 'age_range', 'tell_us_about_yourself', 'created',
                   'personal_traits', 'interests']
     def to_internal_value(self, data):
         return data
+
     def create(self, validated_data):
         interests_data = validated_data.pop('interests') 
         personal_traits_data = validated_data.pop('personal_traits')
@@ -35,3 +36,27 @@ class ListingAccountSerializer(serializers.ModelSerializer):
         for personal_trait_data in personal_traits_data:
             PersonalTrait.objects.create(listing_account = listing_account, **personal_trait_data)
         return listing_account
+
+    def update(self, instance, validated_data):
+        interests = validated_data.pop('interests') 
+        personal_traits = validated_data.pop('personal_traits')
+
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.email = validated_data.get("email", instance.email)
+        instance.phone_number = validated_data.get("data_of_birth", instance.phone_number)
+        instance.location = validated_data.get("location", instance.location)
+        instance.age_range = validated_data.get("age_range", instance.age_range)
+        instance.tell_us_about_yourself = validated_data.get("tell_us_about_yourself", instance.tell_us_about_yourself)
+
+        # # keep_personal_traits = []
+        # # existing_ids = [c.id for c in instance.personal_traits]
+        # # for personal_trait in personal_traits:
+        # #     if "id" in personal_trait.keys():
+        # #         if PersonalTrait.objects.filter(id=personal_trait["id"]).exists():
+        # #             c = PersonalTrait.objects.get(id=personal_trait["id"])
+        # #             c.text = personal_trait.get()
+
+        instance.save()
+        return instance
+
