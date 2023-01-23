c485a2cd13586e12c8eb6d41421f31086c8523c5
Wesley Kim <wesleykim2002@gmail.com>
2023-01-23 11:08:39 -0500

    Added events model with serializer and url

--- BackendAPI/Coliving/urls.py
+++ BackendAPI/Coliving/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('listingAccount.urls')),
+    path('', include('events.urls'))
     
     # path('', TemplateView.as_view(template_name='index.html'))
 ]
