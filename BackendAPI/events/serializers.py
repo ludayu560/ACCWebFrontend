66e8ed5673563dd7d6cb6fbeb150f75031e42a69
Wesley Kim <wesleykim2002@gmail.com>
2023-01-24 12:07:21 -0500

    cleaned up code in serializer

--- BackendAPI/events/serializers.py
+++ BackendAPI/events/serializers.py
@@ -4,7 +4,7 @@ from events.models import Events
 class EventsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Events
-        fields = ['event_image','event_name','event_date_time','event_location','event_description']
+        fields = ['id','event_image','event_name','event_date_time','event_location','event_description']
     def to_internal_value(self, data):
         return data
 
@@ -18,14 +18,6 @@ class EventsSerializer(serializers.ModelSerializer):
         instance.event_date_time = validated_data.get("event_date_time", instance.event_date_time)
         instance.event_location = validated_data.get("event_location", instance.event_location)
         instance.event_description = validated_data.get("event_description", instance.event_description)
-        # # keep_personal_traits = []
-        # # existing_ids = [c.id for c in instance.personal_traits]
-        # # for personal_trait in personal_traits:
-        # #     if "id" in personal_trait.keys():
-        # #         if PersonalTrait.objects.filter(id=personal_trait["id"]).exists():
-        # #             c = PersonalTrait.objects.get(id=personal_trait["id"])
-        # #             c.text = personal_trait.get()
-
+        
         instance.save()
         return instance
-
