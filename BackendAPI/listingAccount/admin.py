0ce34a2526b233ff65e9532814f7caa41b29e008
George Li <georgeli293@gmail.com>
2023-01-25 19:49:58 -0500

    put/patch requests now working

--- BackendAPI/listingAccount/admin.py
+++ BackendAPI/listingAccount/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from listingAccount.models import ListingAccount
-# from listingAccount.models import ListingAccount, PersonalTrait, Interest
+# from listingAccount.models import ListingAccount
+from listingAccount.models import ListingAccount, PersonalTrait, Interest
 # Register your models here.
 
 admin.site.register(ListingAccount)
-# admin.site.register(PersonalTrait)
-# admin.site.register(Interest)
+admin.site.register(PersonalTrait)
+admin.site.register(Interest)
--- BackendAPI/listingAccount/views.py
+++ BackendAPI/listingAccount/views.py
@@ -1,9 +1,6 @@
 # Create your views here.
-# from .serializers import ListingAccountSerializer, InterestsSerializer, PersonalTraitsSerializer
-
-from .models import ListingAccount
-from .serializers import ListingAccountSerializer
-
+from .models import ListingAccount, Interest, PersonalTrait
+from .serializers import ListingAccountSerializer, InterestsSerializer, PersonalTraitsSerializer
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -12,13 +9,13 @@ class ListingAccountViewSet(viewsets.ModelViewSet):
     queryset = ListingAccount.objects.all()
     serializer_class = ListingAccountSerializer
 
-# class InterestsViewSet(viewsets.ModelViewSet):
-#     queryset = Interest.objects.all()
-#     serializer_class = InterestsSerializer
+class InterestsViewSet(viewsets.ModelViewSet):
+    queryset = Interest.objects.all()
+    serializer_class = InterestsSerializer
 
-# class PersonalTraitsViewSet(viewsets.ModelViewSet):
-#     queryset = PersonalTrait.objects.all()
-#     serializer_class = PersonalTraitsSerializer
+class PersonalTraitsViewSet(viewsets.ModelViewSet):
+    queryset = PersonalTrait.objects.all()
+    serializer_class = PersonalTraitsSerializer
 
 class ListAccountGetView(APIView):
     def get(self, request, id):
