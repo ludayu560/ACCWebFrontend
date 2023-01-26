442cc1fe8a0b16205e1a1f6a30ab9dc8143063b4
George Li <georgeli293@gmail.com>
2023-01-26 12:01:45 -0500

    finished notifications

--- BackendAPI/notifications/models.py
+++ BackendAPI/notifications/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Notifications(models.Model):
     message = models.CharField(max_length=200, null=True)
-    link = models.URLField(max_length=128)
+    link = models.URLField(max_length=128, null=True)
 
