408b06b1b0183c6f20a847b8165642a6e05d0aa8
Wesley Kim <wesleykim2002@gmail.com>
2023-01-27 13:19:27 -0500

    Update models.py

--- BackendAPI/notifications/models.py
+++ BackendAPI/notifications/models.py
@@ -6,4 +6,3 @@ class Notifications(models.Model):
     message = models.CharField(max_length=200, null=True)
     link = models.URLField(max_length=128, null=True)
     listing_account = models.ForeignKey(ListingAccount, related_name='notifications', on_delete=models.CASCADE, null=True)
-
