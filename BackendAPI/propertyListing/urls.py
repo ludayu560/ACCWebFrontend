8f346edf136552d5a931cbdcf0ed77029e788ddf
Wesley Kim <71526260+wesleykim2002@users.noreply.github.com>
2023-02-23 16:25:36 -0500

    debugging the 1146 error

--- BackendAPI/propertyListing/urls.py
+++ BackendAPI/propertyListing/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
 
-router=DefaultRouter()
+router = DefaultRouter()
 router.register("PropertyListing", views.PropertyListingViewSet)
 router.register("ListingUtilities", views.ListingUtilitiesViewSet)
 urlpatterns = [
