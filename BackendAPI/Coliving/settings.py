21142fb0a2b5c11ac2657caac7c4960ea2479bb3
Wesley Kim <71526260+wesleykim2002@users.noreply.github.com>
2023-03-01 15:47:40 -0500

    image uploads locally

--- BackendAPI/Coliving/settings.py
+++ BackendAPI/Coliving/settings.py
@@ -88,12 +88,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'production',
-        'USER': 'admin',
-        'PASSWORD': 'Work2003',
-        'HOST': 'coliving-db.ckmvyermclnj.us-west-2.rds.amazonaws.com',
-        'PORT': 3306
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'production',
+        # 'USER': 'admin',
+        # 'PASSWORD': 'Work2003',
+        # 'HOST': 'coliving-db.ckmvyermclnj.us-west-2.rds.amazonaws.com',
+        # 'PORT': 3306
     }
 }
 
@@ -132,7 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- BackendAPI/events/urls.py
+++ BackendAPI/events/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
 
 router=DefaultRouter()
 router.register("Events", views.EventsViewSet)
 urlpatterns = [
     path('', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
