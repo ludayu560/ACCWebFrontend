733ad1b8c11113c8243bcd7eb7b846d7a038bfaf
Shonn Li <77355994+Shonn-Li@users.noreply.github.com>
2023-03-09 09:14:14 +0800

    frontend listingAccount save

--- BackendAPI/account/urls.py
+++ BackendAPI/account/urls.py
@@ -1,6 +1,12 @@
 # from django.contrib import admin
 # from django.urls import path, include
 # from . import views
+# from rest_framework.routers import DefaultRouter
+
+# router = DefaultRouter()
+# router.register("Account", views.AccountViewSet)
+
 # urlpatterns = [
-#     path('get/<string:email>/', views.ListAccountGetView.as_view()),
+#     path('Account/get/<int:id>/', views..as_view()),
+#     path('', include(router.urls)),
 # ]
