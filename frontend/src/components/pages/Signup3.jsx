faa9d1a0aca3fd3bbf71bb93b92a89029123d42b
Shonn Li <77355994+Shonn-Li@users.noreply.github.com>
2023-03-16 10:06:36 +0800

    auth

--- frontend/src/components/pages/Signup3.jsx
+++ frontend/src/components/pages/Signup3.jsx
@@ -9,7 +9,7 @@ import CustomTextField from "../components/CustomTextField";
 import SignupProgressionIcon from "../components/SignupProgressIcon";
 import HouseLogoPink from "../components/HouseLogoPink";
 import axios from "axios";
-import { signup, create_listing_account} from "../../AuthComponents/actions/auth";
+import { signup, create_listing_account } from "../../AuthComponents/actions/auth";
 import { connect } from "react-redux";
 function Page3({
   setPage,
@@ -70,30 +70,30 @@ function Page3({
       traits.map((item) => processedTraits.append({ trait: item }));
       interests.map((item) => processedInterests.append({ interest: item }));
 
-      listingAccount = {
-          username: pageValueOne.username,
-          account_type: pageValueTwo,
-          first_name: pageValueOne.firstName,
-          last_name: pageValueOne.lastName,
-          email: pageValueOne.email,
-          phone_number: "",
-          date_of_birth: null,
-          location: location,
-          age_range: age,
-          occupation: occupation,
-          news_consent: true,
-          tell_us_about_yourself: "",
-          profile_picture: null,
-          banner_picture: null,
-          display_picture_one: null,
-          display_picture_two: null,
-          display_picture_three: null,
-          display_picture_four: null,
-          traits: processedTraits, // we need to process personal traits and interests before they are useable
-          interests: processedInterests,
-          notifications: [],
-          user_id: user.id,
-        }
+      const listingAccount = {
+        username: pageValueOne.username,
+        account_type: pageValueTwo,
+        first_name: pageValueOne.firstName,
+        last_name: pageValueOne.lastName,
+        email: pageValueOne.email,
+        phone_number: "",
+        date_of_birth: null,
+        location: location,
+        age_range: age,
+        occupation: occupation,
+        news_consent: true,
+        tell_us_about_yourself: "",
+        profile_picture: null,
+        banner_picture: null,
+        display_picture_one: null,
+        display_picture_two: null,
+        display_picture_three: null,
+        display_picture_four: null,
+        traits: processedTraits, // we need to process personal traits and interests before they are useable
+        interests: processedInterests,
+        notifications: [],
+        user_id: user.id,
+      }
       create_listing_account(listingAccount)
       setPage(1);
     }
@@ -125,7 +125,7 @@ function Page3({
               variant="signup"
               onClick={(e) => setLocation(e.target.value)}
               label="Location"
-              style={{width:'23vw'}}
+              style={{ width: '23vw' }}
             >
               {" "}
             </CustomTextField>
@@ -140,7 +140,7 @@ function Page3({
               variant="signup"
               onClick={(e) => setOccupation(e.target.value)}
               label="Occupation*"
-              style={{width:'23vw'}}
+              style={{ width: '23vw' }}
             >
               {" "}
             </CustomTextField>
@@ -150,7 +150,7 @@ function Page3({
               onChange={(e) => setDOB(e.target.value)}
               label="Date of Birth"
               type="date"
-              style={{width:'25vw'}}
+              style={{ width: '25vw' }}
             >
               {" "}
             </CustomTextField>
@@ -196,4 +196,4 @@ const mapStateToProps = (state) => ({
   user: state.auth.user,
 });
 
-export default connect(mapStateToProps, { signup, create_listing_account})(Page3);
+export default connect(mapStateToProps, { signup, create_listing_account })(Page3);
