232835edcd3389c86047c7ef643aaa492e72033a
ludayu560 <87355725+ludayu560@users.noreply.github.com>
2023-04-16 23:26:48 -0400

    fix conditional loading

--- frontend/src/components/pages/BlogsDisplay.jsx
+++ frontend/src/components/pages/BlogsDisplay.jsx
@@ -1,65 +1,29 @@
 import { Box, Stack } from "@mui/system";
-import {
-  Button,
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Grid,
-  Icon,
-  Paper,
-  TextField,
-  Typography,
-} from "@mui/material";
-import HomePhoto from "../../assets/HomePhoto.png";
-import NavBar from "../components/NavBar";
-import StyledButton from "../components/StyledButton";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import PaidOutlinedIcon from "@mui/icons-material/PaidOutlined";
-import ForestOutlinedIcon from "@mui/icons-material/ForestOutlined";
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import HouseIcon from "@mui/icons-material/House";
-import PeopleIcon from "@mui/icons-material/People";
-import GppGoodIcon from "@mui/icons-material/GppGood";
-import ClassOutlinedIcon from "@mui/icons-material/ClassOutlined";
-import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
-import VideocamOutlinedIcon from "@mui/icons-material/VideocamOutlined";
-import Footer from "../components/Footer";
-import Mainbar from "../components/MainBar";
-import KathleenLaura from "../../assets/KathleenLaura.png";
-import Kathryn from "../../assets/Kathryn.png";
-import Partner from "../../assets/Partner.png";
-import AishaSignaturePink from "../../assets/AishaSignaturePink.svg";
-import Hands from "../../assets/Hands.png";
-import quote1 from "../../assets/Quote 1.svg";
-import quote2 from "../../assets/quote2.png";
-import img20 from "../../assets/image 20.png";
-import img13 from "../../assets/image 13.png";
-import img12 from "../../assets/image 12.png";
-import img11 from "../../assets/image 11.png";
-import LogoVariant2 from "../../assets/LogoVariant2.svg";
+import { Typography } from "@mui/material";
 import { connect } from "react-redux";
-import SearchBar from "../components/SearchBar";
-import ECard from "../components/ECard";
-import blogStyles from "../pageStyles/blogStyles.css";
 
 function BlogsDisplay({ currentBlog }) {
-
-  return (
+  return currentBlog ? (
     <div>
-      <div  className="acc-blog-section blog-hero ">
-        <div className="blog-header-txt" >
+      <div className="acc-blog-section blog-hero ">
+        <div className="blog-header-txt">
           <a className="btn link-btn">Inspiring Women</a>
-          <Typography variant="h2" fontWeight={"500"}>{currentBlog.Title}</Typography>
-          <p>{currentBlog.created}- {currentBlog.author} - NO COMMENTS</p>
+          <Typography variant="h2" fontWeight={"500"}>
+            {currentBlog.Title}
+          </Typography>
+          <p>
+            {currentBlog.created}- {currentBlog.author} - NO COMMENTS
+          </p>
         </div>
       </div>
 
       <div className="acc-section">
         <div className="acc-blog-container">
           <div className="md-wrapper">
-            <textarea value={currentBlog.markdown_content} style={{ width: "80ch", height: "auto" }}>
-            </textarea>
+            <textarea
+              value={currentBlog.markdown_content}
+              style={{ width: "80ch", height: "auto" }}
+            ></textarea>
           </div>
           <div className="enjoy-read">
             <h3>Enjoy Your Read?</h3>
@@ -74,7 +38,9 @@ function BlogsDisplay({ currentBlog }) {
             </div>
           </div> */}
           <div className="socials-wrapper">
-            <Typography variant="p" fontWeight={"medium"}>Share this:</Typography>
+            <Typography variant="p" fontWeight={"medium"}>
+              Share this:
+            </Typography>
             <div className="socials">
               <a className="btn tag-btn">Twitter</a>
               <a className="btn tag-btn">Facebook</a>
@@ -83,39 +49,52 @@ function BlogsDisplay({ currentBlog }) {
             </div>
           </div>
           <div className="likes">
-            <Typography variant="p" fontWeight={"medium"}>Like this:</Typography>
+            <Typography variant="p" fontWeight={"medium"}>
+              Like this:
+            </Typography>
             <p>Be the first to like this.</p>
           </div>
           <div className="tags">
-            <Typography variant="p" fontWeight={"medium"}> Tags:</Typography>
+            <Typography variant="p" fontWeight={"medium"}>
+              {" "}
+              Tags:
+            </Typography>
             <div className="tags-wrapper">
               <a className="btn tag-btn">{currentBlog.tags}</a>
               <a className="btn tag-btn">WOMAN HOMEOWNER</a>
               <a className="btn tag-btn">SHORT STAYS</a>
-              <a className="btn tag-btn">WOMAN EMPOWERMENT</a >
+              <a className="btn tag-btn">WOMAN EMPOWERMENT</a>
               <a className="btn tag-btn">SHORT-TERM ACCOMODATION</a>
               <a className="btn tag-btn">FINANCIALS</a>
               <a className="btn tag-btn">RURAL AREAS</a>
             </div>
           </div>
           <div className="prev-next-blog">
-            <a className="btn prev-next-btn">&lt;  The Need for Women 50+</a>
-            <a className="btn btn prev-next-btn"> The Need for Women 50+ &gt;</a>
+            <a className="btn prev-next-btn">&lt; The Need for Women 50+</a>
+            <a className="btn btn prev-next-btn">
+              {" "}
+              The Need for Women 50+ &gt;
+            </a>
           </div>
-          <Typography variant="h4" mb={6} fontWeight={"medium"}> Comments</Typography>
-          <div style={{display: "flex", justifyContent: "center"}}>
-            <div className="comments-wrapper" >
+          <Typography variant="h4" mb={6} fontWeight={"medium"}>
+            {" "}
+            Comments
+          </Typography>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <div className="comments-wrapper">
               <div className="comments">
                 <h6>Leave a Reply</h6>
-                <textarea defaultValue={"Enter your comment here..."} style={{ width: "54ch", height: "8rem", padding: "1rem" }}></textarea>
+                <textarea
+                  defaultValue={"Enter your comment here..."}
+                  style={{ width: "54ch", height: "8rem", padding: "1rem" }}
+                ></textarea>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-
-  );
+  ) : null;
 }
 
 const mapStateToProps = (state) => ({
